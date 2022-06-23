fdd8df11033dd7469742f6ba19cb09dba79ac91a
SergKry <necroastral@mail.ru>
2022-06-23 15:06:33 +0400

    добавлен данные по тесту и тесты 2

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Radio {
+package ru.netology.radio;
+
+public class Radio {
+    public int currentNumberRadioStation; // текущий номер р-станции
+
+    public int getCurrentNumberRadioStation() { //вызов данных через доп переменную
+        return currentNumberRadioStation;
+    }
+
+    public void setCurrentNumberRadioStation(int newCurrentNumberRadioStation) {  // прямая передача данных о р-станции
+        if (newCurrentNumberRadioStation < 0) {
+            return; // прекращение при отрицательном значении
+        }
+        if (newCurrentNumberRadioStation > 9) {
+            return; // прекращение запроса при значении больше 9
+        }
+        currentNumberRadioStation = newCurrentNumberRadioStation;
+    }
+    public void nextNumberRadioStation() {    // переключение на плюс
+        if (currentNumberRadioStation < 9) {
+            currentNumberRadioStation = currentNumberRadioStation + 1;
+        } else {
+            currentNumberRadioStation = 0;
+        }
+
+    }
+
+    public void prevNumberRadioStation() { //переключение на минус
+        if (currentNumberRadioStation > 0 && currentNumberRadioStation <= 9) {
+            currentNumberRadioStation = currentNumberRadioStation - 1;
+            //}
+        }
+        else if (currentNumberRadioStation ==0){
+            currentNumberRadioStation = 9;
+        }
+       // if (currentNumberRadioStation <= -1 && currentNumberRadioStation >= 10){
+      //      return;   ограничение на числа до и после не работает
+      //  }
+    }
+
+    int currentSoundVolume;   // текущая громкость
+
+    public int getCurrentSoundVolume() { //вызов данных через доп переменную
+        return currentSoundVolume;
+    }
+
+    public void increaseVolumeSound() {    // увеличение громкости
+        if (currentSoundVolume < 10) {
+            currentSoundVolume = currentSoundVolume + 1;
+        }
+        if (currentSoundVolume >= 10) {
+            currentSoundVolume = 10;
+        }
+    }
+
+    public void decreaseVolumeSound() { //уменьшение громкости
+        if (currentSoundVolume <= 10) {
+            currentSoundVolume = currentSoundVolume - 1;
+        }
+        if (currentSoundVolume <= 0) {
+            currentSoundVolume = 0;
+        }
+    }
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -1,2 +1,238 @@
-package ru.netology.radio;public class RadioTest {
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+
+public class RadioTest {
+
+    // блок прямого ввода значения
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationAverage() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(5);
+
+        int expected = 5;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationMinBorder1() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(-1);
+
+        int expected = 0;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationMinBorder2() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(0);
+
+        int expected = 0;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationMinBorder3() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(1);
+
+        int expected = 1;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationMaxBorder1() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(8);
+
+        int expected = 8;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationMaxBorder2() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(9);
+
+        int expected = 9;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationMaxBorder3() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(10);
+
+        int expected = 0;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationNegativeNotValid() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(-20);
+
+        int expected = 0;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldInputNumberRadioStationNotValidPositiveMeaning() {
+        Radio service = new Radio();
+        service.setCurrentNumberRadioStation(20);
+
+        int expected = 0;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // блок преключение через next
+    // функционал не позволяет использовать значения вне диапозона {0,1,2,3,4,5,6,7,8,9}
+
+    @org.junit.jupiter.api.Test
+    void shouldNextNumberRadioStationMax() {
+        Radio service = new Radio();
+        service.currentNumberRadioStation = 9;
+        service.nextNumberRadioStation();
+
+        int expected = 0;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldNextNumberRadioStationAverage() {
+        Radio service = new Radio();
+        service.currentNumberRadioStation = 5;
+        service.nextNumberRadioStation();
+
+        int expected = 6;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldNextNumberRadioStationMin() {
+        Radio service = new Radio();
+        service.currentNumberRadioStation = 0;
+        service.nextNumberRadioStation();
+
+        int expected = 1;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    // блок преключение через prev
+    // функционал не позволяет использовать значения вне диапозона {0,1,2,3,4,5,6,7,8,9}
+
+    @org.junit.jupiter.api.Test
+    void shouldPrevNumberRadioStationMax() {
+        Radio service = new Radio();
+        service.currentNumberRadioStation = 9;
+        service.prevNumberRadioStation();
+
+        int expected = 8;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldPrevNumberRadioStationAverage() {
+        Radio service = new Radio();
+        service.currentNumberRadioStation = (5);
+        service.prevNumberRadioStation();
+
+        int expected = 4;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldPrevNumberRadioStationMin() {
+        Radio service = new Radio();
+        service.currentNumberRadioStation = (0);
+        service.prevNumberRadioStation();
+
+        int expected = 9;
+        int actual = service.getCurrentNumberRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    // блок увеличения громкости радио
+
+    @org.junit.jupiter.api.Test
+    void shouldIncreaseVolumeSoundMiddle() {
+        Radio service = new Radio();
+        service.currentSoundVolume = 5;
+        service.increaseVolumeSound();
+
+        int expected = 6    ;
+        int actual = service.getCurrentSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldIncreaseVolumeSoundMax() {
+        Radio service = new Radio();
+        service.currentSoundVolume = 10;
+        service.increaseVolumeSound();
+
+        int expected = 10;
+        int actual = service.getCurrentSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldIncreaseVolumeSoundMin() {
+        Radio service = new Radio();
+        service.currentSoundVolume = 0;
+        service.increaseVolumeSound();
+
+        int expected = 1;
+        int actual = service.getCurrentSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // блок уменьшение громкости радио
+    @org.junit.jupiter.api.Test
+    void shouldDecreaseVolumeSoundMiddle() {
+        Radio service = new Radio();
+        service.currentSoundVolume = 5;
+        service.decreaseVolumeSound();
+
+        int expected = 4;
+        int actual = service.getCurrentSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldDecreaseVolumeSoundMax() {
+        Radio service = new Radio();
+        service.currentSoundVolume = 10;
+        service.decreaseVolumeSound();
+
+        int expected = 9;
+        int actual = service.getCurrentSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldDecreaseVolumeSoundMin() {
+        Radio service = new Radio();
+        service.currentSoundVolume = 0;
+        service.decreaseVolumeSound();
+
+        int expected = 0;
+        int actual = service.getCurrentSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
